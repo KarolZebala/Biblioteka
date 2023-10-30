04218d18c5ec7581de568896d37ae194dcc26984
LAPTOP-46C9MTE1\karol <karolze27@gmail.com>
2023-10-30 10:48:16 +0100

    Początkowy komit

---
+++ src/main/java/com/mycompany/biblioteka/Biblioteka.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.biblioteka;
+
+/**
+ *
+ * @author karol
+ */
+public class Biblioteka {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
