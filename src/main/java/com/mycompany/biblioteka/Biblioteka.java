bc1bf82db5f12ad3a9c4d5a2a0fbb41f7f43aa9e
LAPTOP-46C9MTE1\karol <karolze27@gmail.com>
2023-10-30 11:23:43 +0100

    Implementacja klasy zasob

--- src/main/java/com/mycompany/biblioteka/Biblioteka.java
+++ src/main/java/com/mycompany/biblioteka/Biblioteka.java
@@ -4,14 +4,32 @@
 
 package com.mycompany.biblioteka;
 
+import java.util.ArrayList;
 /**
  *
  * @author karol
  */
 public class Biblioteka {
 
-    
+    private static ArrayList<Zasob> _zasoby = new ArrayList<Zasob>();
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        initZasoby();
+        for (Zasob zasob : _zasoby) {
+            System.out.println(zasob.getAutor());
+        }
     }
+    private static void initZasoby(){
+        for (int i = 0; i < 5; i++) {
+            Zasob z = Zasob.UttworzZasob(
+                    false,
+                    "test",
+                    30,
+                    "ja",
+                    2020
+            );
+            _zasoby.add(z);
+        }
+    }
+    
 }
